c4901e41d72e94f647eb2acd3d437d5d0f17e4d3
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-12-20 03:46:44 +0300

    feat: implement Service model for queue categorization

---
+++ backend/services/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Service
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_active", "created_at")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
